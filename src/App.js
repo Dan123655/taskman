30c276cf05fa0c2ab34c3be0bf9b8a4ff23486be
Dan Romanov <chcvlr@protonmail.com>
2022-09-06 20:22:31 +0200

    commit for anton

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import {BsBook,BsEye,BsEyeSlash} from 'react-icons/bs'
 import {AiOutlineCloseCircle} from 'react-icons/ai'
 import {HiOutlinePlus} from 'react-icons/hi'
 function App() {
-const [tasks, setTasks] = useState([]);
+
+
+
+
+const [tasks, setTasks] = useState(()=>{
+  const localData = localStorage.getItem('tasks');
+  return localData ? JSON.parse(localData) : [];
+});
+
+
+
+
+
+
+
 const [input, setInput] = useState('')
-
 const [popped, setPopped] = useState(false)
-
-
-
 const showOneTimeThenDont=(e)=>{
-
-setTimeout(()=>{setPopped(true); console.log('just popped or double clicked')}, 2500)}
-  
-
-
-
-
+  setTimeout(()=>{setPopped(true); console.log('just popped or double clicked')}, 2500)}
 const [hideCompleted, setHideCompleted] = useState(false);
 const toggleHideCompleted =()=>{
   setHideCompleted(current=>!current)
   console.log("switched hideCompleted to "+ hideCompleted)
 }
 
+useEffect(() =>{localStorage.setItem('tasks', JSON.stringify(tasks))}, [tasks])
 
-const handleSumbit=(e)=>{
+
+const HandleSumbit=(e)=>{
   e.preventDefault()
   const addTask = {
     id: Math.floor(Math.random()* 10000),
     text: input,
     completed: false
   }
-  setTasks([...tasks, addTask]);
-  setInput('')
+  setTasks([...tasks, addTask],);
+  setInput('');
 }
 
 
@@ -43,12 +49,11 @@ const handleSumbit=(e)=>{
 
 //delete task
 const deleteTask=(id)=>{
-  let filteredTasks=[...tasks].filter((tasks)=>tasks.id !==id)
+  let filteredTasks=[...tasks].filter((tasks)=>tasks.id !==id);
+  // useEffect((tasks) =>{localStorage.setItem('tasks', JSON.stringify(tasks))}, [tasks])
   setTasks(filteredTasks)
   console.log('deleted ok')
 }
-
-
 //toggle completed task
 const toggleComplete= (id,e)=>{showOneTimeThenDont(e); 
   setTasks(tasks.map(task=>task.id===id?{...task, completed: !task.completed} : task))
@@ -62,6 +67,20 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
 const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <div className='app-title'>
@@ -70,75 +89,31 @@ const days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 
       <div className='date'>
-        
-        {/* <p>{date.getFullYear()}</p> */}
-        <p>{!hideCompleted?<BsEye className='hide-completed' onClick={toggleHideCompleted}/>:<BsEyeSlash className='hide-completed' color='gray' onClick={toggleHideCompleted}/>}</p>   
+            <p>{!hideCompleted?<BsEye className='hide-completed' onClick={toggleHideCompleted}/>:<BsEyeSlash className='hide-completed' color='gray' onClick={toggleHideCompleted}/>}</p>   
+      </div>
+      <form onSubmit={HandleSumbit}>
+            <div className='form-input'>
+          <HiOutlinePlus className='icon-add' onClick={HandleSumbit}/>
+            <input 
+            placeholder='Enter a task..'
+            type='text'
+            value={input}
+            maxLength="33"
+            onChange={e => setInput(e.target.value)} />
+            </div>
+      </form>
+    {(tasks.length==1)&&!popped?(<div className='popup-task-row' ><p className='popup-textline' onLoad={showOneTimeThenDont()}>double tap to complete</p></div>):<></>}
+    
+    {tasks.map(task=>{
+      if(hideCompleted){
+        if(!task.completed)return (
+          <div className={`${task.completed? 'completed' : 'task-row'}`} key={task.id} onDoubleClick={(e) => {toggleComplete(task.id)}}><p className='textline'>{task.text}</p>
+            <AiOutlineCloseCircle className='icon-delete' onClick={()=>deleteTask(task.id)}/>
+          </div>)}
+        else{return (<div className={`${task.completed? 'completed' : 'task-row'}`} key={task.id} onDoubleClick={(e) =>{toggleComplete(task.id)}}><p className='textline'>{task.text}</p>
+          <AiOutlineCloseCircle className='icon-delete' onClick={()=>deleteTask(task.id)}/></div>)
 
-        </div>
-        
-
-
-
-        <form onSubmit={handleSumbit}>
-          <div className='form-input'>
-        <HiOutlinePlus className='icon-add' onClick={handleSumbit}/>
-          <input 
-          placeholder='Enter a task..'
-          type='text'
-          value={input}
-          maxLength="33"
-          onChange={e => setInput(e.target.value)} />
-        </div>
-
-        </form>
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-            {(tasks.length==1)&&!popped?(<div className='popup-task-row' ><p className='popup-textline' onLoad={showOneTimeThenDont()}>double tap to complete
-            </p></div>):<></>}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {tasks.map(task=>{
-          
-          if(hideCompleted){
-                      if(!task.completed)return (
-
-                        <div className={`${task.completed? 'completed' : 'task-row'}`} key={task.id} onDoubleClick={(e) => {toggleComplete(task.id)}}><p className='textline'>{task.text}</p>
-                              <AiOutlineCloseCircle className='icon-delete' onClick={()=>deleteTask(task.id)}/>
-                        </div>)
-      
-          }else{return (<div className={`${task.completed? 'completed' : 'task-row'}`} key={task.id} onDoubleClick={(e) =>{toggleComplete(task.id)}}><p className='textline'>{task.text}</p>
-                              <AiOutlineCloseCircle className='icon-delete' onClick={()=>deleteTask(task.id)}/>
-                </div>
-            )
-
-          }
-      
-      }
-        
-      
-        ).reverse()}
+            }}).reverse()}
 
 
       </div>
